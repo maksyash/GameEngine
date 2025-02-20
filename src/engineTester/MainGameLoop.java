e3b05faaa82a4a772467e2a0e776efc1204c2913
maksyash <friendlymaxmail@gmail.com>
2025-02-20 20:09:47 +0400

    init

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,58 @@
+package engineTester;
+
+import models.TexturedModel;
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import models.RawModel;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+
+public class MainGameLoop {
+
+    public static void main(String[] args) {
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
+
+        float[] vertices = {
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+        };
+        int[] indices = {
+                0,1,3,
+                3,1,2
+        };
+
+        float[] textureCoords = {
+                0,0,
+                0,1,
+                1,1,
+                1,0
+        };
+
+
+        RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("bobik"));
+        TexturedModel texturedModel = new TexturedModel(model, texture);
+
+        while (!Display.isCloseRequested()) {
+            // game logic
+            renderer.prepare();
+            shader.start();
+            renderer.render(texturedModel);
+            shader.stop();
+            DisplayManager.updateDisplay();
+        }
+
+        shader.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+    }
+
+}
